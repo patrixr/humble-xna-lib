3dc24ea382dfe13d20ebc081f7e13ba27d54bff3
Patrick Rabier <patrick.rabier@gmail.com>
2012-06-06 16:47:10 +0200

    Post Delayed Message Screen and Message documentation New HumbleTest message test.

---
+++ HumbleXnaLibrary/Configuration/HumbleConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Humble.Configuration
+{
+    /// <summary>
+    /// Contains properties that define the general behavior of Humble
+    /// </summary>
+    public static class HumbleConfiguration
+    {
+        /// <summary>
+        /// Defines the maximum number of messages processed every frame
+        /// Set to 0 to process all of them every time.
+        /// </summary>
+        public static uint MSG_MAX_PROCESSED_MESSAGES = 0;
+    }
+}
